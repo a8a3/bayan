13d2a0dd33abd53c6659a4936cab4a4092870130
Alexey Abramov <a.abramov@egopay.ru>
2019-11-22 15:35:23 +0300

    read command-line parameters to struct

--- sources/bayan.cpp
+++ sources/bayan.cpp
@@ -1,7 +1,42 @@
-#include <iostream>
 
+
+#include <boost/program_options.hpp>
+#include <boost/program_options/options_description.hpp>
+
+#include "options.hpp"
+
+namespace po = boost::program_options;
+bayan::options o;
 
 // ------------------------------------------------------------------
-int main(int, char**) {
+int main(int argc, char** argv) {
+   try {
+      po::options_description opt_desc("command line options");
+      opt_desc.add_options()
+            ("help,h", "produce help message")
+            ("include-dirs,I", po::value<std::vector<std::string>>(&o.in_dirs_),     "include directories for scan")
+            ("exclude-dirs,E", po::value<std::vector<std::string>>(&o.excl_dirs_),   "exclude directories for scan")
+            ("recursive,R",    po::value<int>(&o.recursive_),                        "0- scan current directory, 1- recursive scan")
+            ("min-size,B",     po::value<int>(&o.min_file_sz_),                      "min file size(bytes) to scan")
+            ("file-mask,M",    po::value<std::vector<std::string>>(&o.files_masks_), "files names masks to scan")
+            ("block-size,S",   po::value<int>(&o.block_sz_),                         "scan block size(bytes)")
+            ("conv,C",         po::value<std::string>(&o.conv_algo_),                "convolution algorithm")
+      ;
+
+      po::variables_map vm;
+      po::store(po::parse_command_line(argc, argv, opt_desc), vm);
+      po::notify(vm);
+
+      if (vm.count("help")) {
+         std::cout << opt_desc << '\n';
+         return 1;
+      }
+
+      std::cout << o;
+   }
+   catch (const po::error& e) {
+      std::cerr << e.what() << std::endl;
+   }
+      
    return 0;
 }
---
+++ sources/options.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+namespace bayan {
+// ------------------------------------------------------------------
+struct options {
+   std::vector<std::string> in_dirs_;
+   std::vector<std::string> excl_dirs_;
+   std::vector<std::string> files_masks_;
+   std::string              conv_algo_{"md5"};
+
+   int min_file_sz_{1};
+   int recursive_  {0};
+   int block_sz_   {1};
+};
+
+// ------------------------------------------------------------------
+std::ostream& operator << (std::ostream& os, const bayan::options& opt) {
+   os << "scan parameters:\n";
+   const auto print_str_list = [&os] (const auto head, const auto& list) {
+
+      if (list.empty()) {
+         os << head << " none\n";
+         return;
+      }
+      os << head << '\n';
+      for( const auto& item: list ) {
+         os << item << '\n';
+      }
+   };
+
+   print_str_list("include dirs: ", opt.in_dirs_);
+   print_str_list("exclude dirs: ", opt.excl_dirs_);
+   print_str_list("files masks:  ", opt.files_masks_);
+
+   os << "conv algo:     " << opt.conv_algo_   << '\n';
+   os << "recursive:     " << opt.recursive_   << '\n';
+   os << "block size:    " << opt.block_sz_    << '\n';
+   os << "min file size: " << opt.min_file_sz_ << '\n';
+
+   return os;
+}
+
+} // namespace bayan
