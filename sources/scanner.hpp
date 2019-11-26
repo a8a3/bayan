9d16864a7a03b0290fe2651d2fe0cd67ba6c49f2
Alexey Abramov <alexpoligon@mail.ru>
2019-11-26 22:11:17 +0300

    start scan implementation

--- sources/scanner.hpp
+++ sources/scanner.hpp
@@ -30,6 +30,8 @@ public:
       if (files_to_scan.empty()) {
          return;
       }
+
+      do_scan(files_to_scan);
    }
 
    std::vector<std::string> get_all_files() const {
@@ -80,6 +82,18 @@ private:
       if (options_.recursive_ == 0 || options_.excl_dirs_.size()) {
          filter_->set_next_filter(std::make_unique<directories_filter>(options_.excl_dirs_, options_.recursive_));
       }
+   }
+
+// 0 1 2 3 4 5    0 2 4   1 3 5
+// A A A A A A    A A A   A A A
+// B D B D B D => B B B   D D D
+// C E H F C F    C H C , E F F
+
+
+
+   void do_scan(const std::vector<std::string>& files_to_scan) const {
+      std::cout << "attempt to scan " << files_to_scan.size() << " files\n";
+
    }
 
    const bayan::options& options_;
